c6caea5091c07d7eb9947449ca7c9f0291d45176
frontenddevmichael <frontenddevmichael@gmail.com>
2026-03-26 08:37:12 +0100

    testing

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,6 +1,9 @@
 import { createClient } from "@supabase/supabase-js";
+const supabaseUrl = "https://sbvfeovsndbrdnpfzmqg.supabase.co";
+const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNidmZlb3ZzbmRicmRucGZ6bXFnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzQxOTYwMDgsImV4cCI6MjA4OTc3MjAwOH0.gtbvMyTge9FKBJg9e8q6v4a3oQqK1_nluDnMujZfd50";
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseAnnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+if (!supabaseUrl || !supabaseAnonKey) {
+    console.error("ENV ERROR:", { supabaseUrl, supabaseAnonKey });
+}
 
-export const supabase = createClient(supabaseUrl , supabaseAnnonKey)
+export const supabase = createClient(supabaseUrl, supabaseAnonKey);
